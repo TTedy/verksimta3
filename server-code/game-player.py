8cee091e213431ea247289c0f07f9f39d11876a5
TTedy <breki121212@gmail.com>
2023-04-28 13:06:23 +0000

    26/5

---
+++ server-code/game-player.py
@@ -0,0 +1,11 @@
+# skrá til þess að halda um spilara. t.d hver á að spila. hvað gerðist.
+# þetta mun vera skrá sem keyrir alltaf meðan serverin er í gangi.
+# serverinn er rasberry pi 4 4gb með 32gb minni. er ekki vis ef þetta er B eða hvað varan
+
+import datetime
+
+print(datetime)
+
+run = True
+
+    
